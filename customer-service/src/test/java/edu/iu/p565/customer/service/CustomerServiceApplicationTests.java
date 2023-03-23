15363dd91c118c346657031f5e839cadefea569d
Zachariah69 <124943206+Zachariah69@users.noreply.github.com>
2023-03-23 16:20:01 -0400

    Add files via upload

---
+++ customer-service/src/test/java/edu/iu/p565/customer/service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.p565.customer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
